ab7e12ce140752bd1eadb864194ac0c4f39b37e2
louisea <louiseamelie2@gmail.com>
2022-08-17 16:43:59 +0200

    Exercice done

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:23:23 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/08/17 14:31:34 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/08/17 16:43:14 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,20 +31,22 @@ std::string	getFileContent(std::string fileName, int *size) {
 
 	infile.open(fileName.c_str(), std::ifstream::in); // open file
 
-	if (infile) // find file length
+	if (infile.is_open()) // find file length
 	{
 		infile.seekg(0, infile.end); // move position to end of file
 		*size = infile.tellg(); // get position number (position nb = filse *size) and store it
 		infile.seekg(0, infile.beg); // move back to beginning of file
+
+		str = new char [*size + 1];
+		infile.read(str, *size); // store file content in str
+
+		s3 = convertToString(str, *size);
+
+		infile.close();
+		delete [] str;
 	}
-
-	str = new char [*size + 1];
-	infile.read(str, *size); // store file content in str
-
-	s3 = convertToString(str, *size);
-
-	infile.close();
-	delete [] str;
+	else
+		std::cout << "Error while openning the file !" << std::endl;
 
 	return (s3);
 }
@@ -58,8 +60,8 @@ void	replaceStringInNewFile(std::string fileName, std::string s1, std::string s2
 	int				size = 0;
 
 	s3 = getFileContent(fileName, &size);
-
-	std::cout << "S3 before replacement: " << s3 << std::endl;
+	if (s3 == "")
+		return ;
 
 	while ((int)s3.find(s1) != -1) // find all occurence of s1 and change it for s2
 	{
@@ -68,11 +70,15 @@ void	replaceStringInNewFile(std::string fileName, std::string s1, std::string s2
 		s3.insert(position, s2);
 	}
 
-	std::cout << "S3 after replacement: " << s3 << std::endl;
-
 	outfile.open((fileName + ".replace").c_str(), std::ofstream::out); // open & create outfile
-	outfile.write(s3.c_str(), size); // write new content in s3
-	outfile.close();
+	if (outfile.is_open())
+	{
+		size = s3.length();
+		outfile.write(s3.c_str(), size); // write new content in s3
+		outfile.close();
+	}
+	else
+		std::cout << "Error while opening/creating the outfile" << std::endl;
 }
 
 int	main(int argc, char **argv) {
