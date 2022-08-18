c972aaad29c6428fb9d0598f82036802f411d737
louisea <louiseamelie2@gmail.com>
2022-08-18 14:48:28 +0200

    main update

--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 17:02:17 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/08/18 14:44:30 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/08/18 14:48:22 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ int	main(void) {
 
 	Harl	yo;
 
+	std::cout << "Debug level :" << std::endl;
 	std::cout << "I love having extra bacon for my burger. I really do!" << std::endl;
 	yo.complain("debug");
 
+	std::cout << "Info level :" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money!" << std::endl;
 	yo.complain("info");
 
+	std::cout << "Warning level :" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
 	yo.complain("warning");
 
+	std::cout << "Error level :" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 	yo.complain("error");
 
