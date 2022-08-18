ff9fbd9737c46da2651e187a1720a47e8c8a7e82
louisea <louiseamelie2@gmail.com>
2022-08-18 15:42:15 +0200

    ex01 done

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -6,13 +6,14 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 14:08:31 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/08/12 14:27:32 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/08/18 15:38:05 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie*	zombieHorde(int N, std::string name) {
+
 	Zombie	*zombies;
 	int		i;
 
@@ -22,8 +23,8 @@ Zombie*	zombieHorde(int N, std::string name) {
 	while (i < N)
 	{
 		zombies[i].setZombieName(name);
-		zombies[i].announce();
 		i++;
 	}
+
 	return (zombies);
 }
