09db65e8a766818e796a14dbfd373b54915a15d7
louisea <louiseamelie2@gmail.com>
2022-10-03 17:14:27 +0200

    changed ex00 main to be more visual

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/30 16:49:57 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/10/03 12:17:52 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/10/03 17:13:43 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,30 @@
 
 int	main() {
 
+	std::cout << "-------- Constructors ---------" << std::endl;
+	std::cout << "Animal: " << std::endl;
 	const Animal* meta = new Animal();
+	std::cout << std::endl;
+	std::cout << "Cat: " << std::endl;
 	const Animal* i = new Cat();
+	std::cout << std::endl;
+	std::cout << "Dog: " << std::endl;
 	const Animal* j = new Dog();
-
+	std::cout << std::endl;
+	std::cout << "Wrong Animal: " << std::endl;
 	const WrongAnimal* beta = new WrongAnimal();
+	std::cout << std::endl;
+	std::cout << "Wrong Cat: " << std::endl;
 	const WrongAnimal* k = new WrongCat();
+	std::cout << "-------------------------------" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+	std::cout << "Dog->getType(): " << j->getType() << " " << std::endl;
+	std::cout << "Cat->getType(): " << i->getType() << " " << std::endl;
+	std::cout << std::endl;
 
 	std::cout << "Cat Sound: ";
-	i->makeSound();//will output the cat sound!
+	i->makeSound();
 	std::cout << "Dog Sound: ";
 	j->makeSound();
 	std::cout << "Animal Sound: ";
@@ -40,11 +52,24 @@ int	main() {
 	std::cout << "Wrong Cat making wrong animal sound: ";
 	k->makeSound();
 
+	std::cout << std::endl;
+	std::cout << "-------- Deconstructors ---------" << std::endl;
+	std::cout << "Animal: " << std::endl;
 	delete meta;
+	std::cout << std::endl;
+	std::cout << "Cat: " << std::endl;
 	delete i;
+	std::cout << std::endl;
+	std::cout << "Dog: " << std::endl;
 	delete j;
+	std::cout << std::endl;
+	std::cout << "Wrong Animal: " << std::endl;
 	delete beta;
+	std::cout << std::endl;
+	std::cout << "Wrong Cat: " << std::endl;
 	delete k;
+	std::cout << "---------------------------------" << std::endl;
+
 
 return (0);
 }
