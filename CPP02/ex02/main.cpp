95cbb87f09fc9dac6fc7163cb1c6b68d50d12e21
louisea <louiseamelie2@gmail.com>
2022-10-04 16:29:02 +0200

    done

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 14:08:14 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/09/29 17:09:45 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/10/04 16:26:13 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,42 @@ int main(void)
 {
 	Fixed		a;
 	Fixed const	b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << std::endl;
 
-	std::cout << a << " -> ";
+	std::cout << "a = " << a << " -> ";
 	std::cout << "Result should be :";
 	std::cout << "0" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << ++a << " -> ";
+	std::cout << "++a = " << ++a << " -> ";
 	std::cout << "Result should be :";
 	std::cout << "0.00390625" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << a << " -> ";
+	std::cout << "a = " << a << " -> ";
 	std::cout << "Result should be :";
 	std::cout << "0.00390625" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << a++ << " -> ";
+	std::cout << "a++ \n" << a++ << std::endl;
 	std::cout << "Result should be :";
 	std::cout << "0.00390625" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << a << " -> ";
+	std::cout << "a = " << a << " -> ";
 	std::cout << "Result should be :";
 	std::cout << "0.0078125" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << b << " -> ";
+	std::cout << "b = " << b << " -> ";
 	std::cout << "Result should be :";
 	std::cout << "10.1016" << std::endl;
+	std::cout << std::endl;
 
-	std::cout << Fixed::max( a, b ) << " -> ";
+	std::cout << "Fixed::max( a, b ) = " << Fixed::max( a, b ) << " -> ";
 	std::cout << "Result should be :";
 	std::cout << "10.1016" << std::endl;
+	std::cout << std::endl;
 
 	return (0);
 }
