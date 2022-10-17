54c7c2423e4147912491129f9cdc43b5758325b6
louisea <louiseamelie2@gmail.com>
2022-10-17 16:09:56 +0200

    parsing ok bb

--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 16:16:32 by lmarecha          #+#    #+#             */
-/*   Updated: 2022/10/13 17:16:01 by lmarecha         ###   ########.fr       */
+/*   Updated: 2022/10/17 16:08:44 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,31 @@ bool	isChar( char *argv ) {
 	return ( false );
 }
 
+bool	isFloat( char *argv ) {
+
+	int	i = 0;
+
+	if ( !isDigit(argv[i]) )
+		return ( false );
+	while ( isDigit(argv[i]) )
+		i++;
+	if ( argv[i] == 46 && isDigit(argv[i + 1]))
+		i++;
+	else
+		return ( false );
+	while ( argv[i] && argv[i] != 'f' )
+	{
+		if ( isDigit(argv[i]) )
+			i++;
+		else
+			return ( false );
+	}
+	if ( argv[i] == 'f' && argv[i + 1] == '\0' )
+		return ( true );
+
+	return ( false );
+}
+
 int	main(int argc, char **argv) {
 
 	if ( argc != 2 )
@@ -92,10 +117,15 @@ int	main(int argc, char **argv) {
 		return (1);
 	}
 
+	if ( !isInt(argv[1]) && !isChar(argv[1]) && !isDouble(argv[1]) && !isFloat(argv[1]) )
+		std::cout << "Wrong input format !" << std::endl;
+
 	if ( isInt(argv[1]) == true )
 		std::cout << argv[1] << " has type int" << std::endl;
 	else if ( isChar(argv[1]) )
 		std::cout << argv[1] << " has type char" << std::endl;
 	else if ( isDouble(argv[1]) )
 		std::cout << argv[1] << " has type double" << std::endl;
+	else if ( isFloat(argv[1]) )
+		std::cout << argv[1] << " has type float" << std::endl;
 }
