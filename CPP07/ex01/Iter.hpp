abdc640731bbbf18d3c62e49cc36f5010a3e6327
louisea <louiseamelie2@gmail.com>
2022-10-25 17:22:53 +0200

    ex01 done

--- CPP07/ex01/Iter.hpp
+++ CPP07/ex01/Iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: louisea <louisea@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 22:03:29 by louisea           #+#    #+#             */
-/*   Updated: 2022/10/23 22:08:26 by louisea          ###   ########.fr       */
+/*   Updated: 2022/10/25 17:20:14 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@
 #define GREEN       "\033[32m"
 #define YELLOW      "\033[33m"
 #define BLUE        "\033[34m"
-#define MAGENTA     "\033[1m\033[35m"
+#define MAGENTA     "\033[35m"
 #define CYAN        "\033[36m"
 #define WHITE       "\033[37m"
 #define BOLDVIOLET  "\033[1m\033[35m"
 #define BOLDBLUE    "\033[1m\033[34m"
 
 template< typename T >
-void	iter( T &a, int size, void fct(T *a) );
+void	iter( T *a, int size, void (*fct)(T a) );
 
 #endif
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -3,17 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: louisea <louisea@student.42.fr>            +#+  +:+       +#+        */
+/*   By: lmarecha <lmarecha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 22:03:08 by louisea           #+#    #+#             */
-/*   Updated: 2022/10/23 22:16:26 by louisea          ###   ########.fr       */
+/*   Updated: 2022/10/25 17:22:30 by lmarecha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Iter.hpp"
 
-void	iter( T &a, int size, void fct() ) {
+template< typename T >
+void	printContent( T a ) {
 
-	for ( i = 0; i < size; i++ )
+	std::cout << a << std::endl;
+}
+
+template< typename T >
+void	iter( T *a, int size, void (*fct)(T a) ) {
+
+	;
+	for ( int i = 0; i < size; i++ )
 		fct(a[i]);
 }
+
+int	main ( void ) {
+
+	int			x[4] 		= { 1, 2, 3, 4 };
+	std::string	strTab[4]	= { "Hello", "Bonjour", "Hola", "NULL" };
+	char		str[]		= "Comment ca va ?";
+
+	std::cout << BOLDVIOLET << "-----ITER ON INT-------" << RESET << std::endl;
+	std::cout << MAGENTA << "int x[4] = { 1, 2, 3, 4 };" << RESET << std::endl;
+	
+	iter(x, 4, printContent);
+	
+	std::cout << BOLDVIOLET << "-----------------------" << RESET << std::endl;
+	std::cout << BOLDVIOLET << "-----ITER ON STRING----" << RESET << std::endl;
+	std::cout << MAGENTA << "std::string	strTab[4] = { 'Hello', 'Bonjour', 'Hola', 'NULL' };" << RESET << std::endl;
+	
+	iter(strTab, 3, printContent);
+	
+	std::cout << BOLDVIOLET << "-----------------------" << RESET << std::endl;
+	std::cout << BOLDVIOLET << "-----ITER ON CHAR----" << RESET << std::endl;
+	std::cout << MAGENTA << "char str[] = 'Comment ca va ?';" << RESET << std::endl;
+	
+	iter(str, 15, printContent);
+	
+	std::cout << BOLDVIOLET << "-----------------------" << RESET << std::endl;
+	return (0);
+}
+
+
